09e05f809512f8f0ce567684bd01dd6b8bcab324
Dorothy Akoth <79142184+Dorothy2020@users.noreply.github.com>
2022-02-20 18:28:06 +0300

    Add files via upload

---
+++ HashMap.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hashmap;
+import java.util.HashMap();
+
+public class HashMap {
+
+    
+    public static void HashMap(String[] args) {
+        // create a hashmap called CapitalCities
+        HashMap<String,String>CapitalCities = new HashMap<String,String>();
+        CapitalCities.put("England","London");
+        System.out.println(CapitalCities);
+        
+    }
+    
+}
